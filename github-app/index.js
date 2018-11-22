bd62d243ffe09f6041206ef0dea6112aeb9e69ea
martinch-kth <martinch@kth.se>
2018-11-22 22:58:50 +0000

    ...

--- github-app/index.js
+++ github-app/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config()  // not really working.. .env file must be in project root
 
 const secret = process.env.WEBHOOK_SECRET
 
@@ -12,7 +12,7 @@ const app = require('github-app')({
   cert: require('fs').readFileSync('testdescartes.2018-11-12.private-key.pem')
 })
 
-http.createServer(handleRequest).listen(3000)
+http.createServer(handleRequest).listen(5000)
 
 webHookHandler.on('issues', (event) => {
   // ignore all issue events other than new issue opened
@@ -34,31 +34,3 @@ function handleRequest (request, response) {
   webHookHandler(request, response, () => response.end('ok'))
 }
 
-/*
-
-// FUNKAR! basic...
-
-// http is a standard module that comes with Node.js
-const http = require('http')
-
-http.createServer(handleRequest).listen(3000)
-
-function handleRequest (request, response) {
-
-  // log request method & URL
-  console.log(`${request.method} ${request.url}`)
-
-  // for GET (and other non-POST) requests show "ok" and stop here
-  if (request.method !== 'POST') return response.end('ok')
-
-  // for POST requests, read out the request body, log it, then show "ok" as response
-  let payload = ''
-  request.on('data', (data) => payload += data );
-  request.on('end', () => {
-    console.log(payload)
-    response.end('ok')
-  })
-}
-
-
-*/

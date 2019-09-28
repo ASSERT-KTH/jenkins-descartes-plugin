da145fb7ab9c1b59374b2a42614fb6fd8edbfa2f
martinch-kth <martinch@kth.se>
2019-09-28 15:51:34 +0200

    ..

--- probot-app/index.js
+++ probot-app/index.js
@@ -36,7 +36,7 @@ module.exports = app => {
 
         // if we in the future need alter jenkins jobs..
        /*
-       if (context.payload.repository.name === "testaaaaaaaaaaaaaa")
+       if (context.payload.repository.name === "testaaaaaaaaaaaa")
        {
            jenkins.job.build({name: 'test-dhell' }, function(err) {
                if (err) throw err;

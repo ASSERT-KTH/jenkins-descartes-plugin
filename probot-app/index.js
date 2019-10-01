5dc619036219be8e5ed6b2c8889aea218c737138
martinch-kth <martinch@kth.se>
2019-10-01 22:49:38 +0200

    ..

--- probot-app/index.js
+++ probot-app/index.js
@@ -17,7 +17,7 @@ db.once('open', function() {
     console.log('db connected...')
 });
 
-var jenkins_project_name = 'test_development'  // test
+var jenkins_project_name = 'test'  // test
 
 // SAVE for re-authentication
 var my_context
